f2d41a62430768f0ee87279e71bd3a667fbd5ce4
Damir Safin <safin24damir@gmail.com>
2020-05-25 22:31:58 +0600

    sssss

--- Workman/CallToSclad.Designer.cs
+++ Workman/CallToSclad.Designer.cs
@@ -28,36 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.CallFormMarka = new System.Windows.Forms.ComboBox();
+            this.CallFormTip = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.CallFormModel = new System.Windows.Forms.ComboBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.CloseCallToSclad = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SformirovatZapros = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // CallFormMarka
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(218, 37);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 1;
+            this.CallFormMarka.FormattingEnabled = true;
+            this.CallFormMarka.Location = new System.Drawing.Point(218, 37);
+            this.CallFormMarka.Name = "CallFormMarka";
+            this.CallFormMarka.Size = new System.Drawing.Size(121, 21);
+            this.CallFormMarka.TabIndex = 1;
             // 
-            // comboBox2
+            // CallFormTip
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(345, 37);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 2;
+            this.CallFormTip.FormattingEnabled = true;
+            this.CallFormTip.Location = new System.Drawing.Point(345, 37);
+            this.CallFormTip.Name = "CallFormTip";
+            this.CallFormTip.Size = new System.Drawing.Size(121, 21);
+            this.CallFormTip.TabIndex = 2;
             // 
             // label1
             // 
@@ -95,13 +94,13 @@
             this.label4.TabIndex = 7;
             this.label4.Text = "Модель";
             // 
-            // comboBox4
+            // CallFormModel
             // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(218, 81);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(121, 21);
-            this.comboBox4.TabIndex = 8;
+            this.CallFormModel.FormattingEnabled = true;
+            this.CallFormModel.Location = new System.Drawing.Point(218, 81);
+            this.CallFormModel.Name = "CallFormModel";
+            this.CallFormModel.Size = new System.Drawing.Size(121, 21);
+            this.CallFormModel.TabIndex = 8;
             // 
             // listBox1
             // 
@@ -115,7 +114,7 @@
             // 
             this.CloseCallToSclad.AutoSize = true;
             this.CloseCallToSclad.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.CloseCallToSclad.Location = new System.Drawing.Point(567, 425);
+            this.CloseCallToSclad.Location = new System.Drawing.Point(564, 298);
             this.CloseCallToSclad.Name = "CloseCallToSclad";
             this.CloseCallToSclad.Size = new System.Drawing.Size(41, 13);
             this.CloseCallToSclad.TabIndex = 10;
@@ -130,7 +129,7 @@
             // 
             // SformirovatZapros
             // 
-            this.SformirovatZapros.Location = new System.Drawing.Point(248, 253);
+            this.SformirovatZapros.Location = new System.Drawing.Point(248, 168);
             this.SformirovatZapros.Name = "SformirovatZapros";
             this.SformirovatZapros.Size = new System.Drawing.Size(243, 23);
             this.SformirovatZapros.TabIndex = 12;
@@ -138,41 +137,35 @@
             this.SformirovatZapros.UseVisualStyleBackColor = true;
             this.SformirovatZapros.Click += new System.EventHandler(this.SformirovatZapros_Click);
             // 
-            // label5
+            // label6
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(374, 65);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(50, 13);
-            this.label5.TabIndex = 13;
-            this.label5.Text = "Артикул";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(346, 81);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(120, 22);
-            this.textBox2.TabIndex = 14;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(68, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Список товара";
             // 
             // CallToSclad
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(617, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label5);
+            this.ClientSize = new System.Drawing.Size(620, 320);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.SformirovatZapros);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.CallFormModel);
             this.Controls.Add(this.CloseCallToSclad);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.CallFormTip);
+            this.Controls.Add(this.CallFormMarka);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(620, 320);
+            this.MinimumSize = new System.Drawing.Size(620, 320);
             this.Name = "CallToSclad";
             this.Text = "CallToSclad";
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.CallToSclad_MouseDown);
@@ -184,18 +177,17 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox CallFormMarka;
+        private System.Windows.Forms.ComboBox CallFormTip;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox CallFormModel;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label CloseCallToSclad;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button SformirovatZapros;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
     }
 }
