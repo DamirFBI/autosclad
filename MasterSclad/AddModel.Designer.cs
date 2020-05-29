e2287cff93e31209085e104f1d936c1bc4fe59f7
Damir Safin <safin24damir@gmail.com>
2020-05-29 17:38:58 +0600

    Добавил фукнцианал и интерфейс

---
+++ MasterSclad/AddModel.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace AutoSclad.MasterSclad
+{
+    partial class AddModel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Close = new System.Windows.Forms.Label();
+            this.AddModelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Модель";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 75);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(476, 273);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Подробности";
+            // 
+            // Close
+            // 
+            this.Close.AutoSize = true;
+            this.Close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Close.Location = new System.Drawing.Point(457, 360);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(51, 13);
+            this.Close.TabIndex = 4;
+            this.Close.Text = "Закрыть";
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // AddModelButton
+            // 
+            this.AddModelButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AddModelButton.Location = new System.Drawing.Point(16, 355);
+            this.AddModelButton.Name = "AddModelButton";
+            this.AddModelButton.Size = new System.Drawing.Size(75, 23);
+            this.AddModelButton.TabIndex = 5;
+            this.AddModelButton.Text = "Добавить";
+            this.AddModelButton.UseVisualStyleBackColor = true;
+            this.AddModelButton.Click += new System.EventHandler(this.AddModelButton_Click);
+            // 
+            // AddModel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 385);
+            this.Controls.Add(this.AddModelButton);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AddModel";
+            this.Text = "AddModel";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.AddModel_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.AddModel_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Close;
+        private System.Windows.Forms.Button AddModelButton;
+    }
+}
---
+++ MasterSclad/AddModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace AutoSclad.MasterSclad
+{
+    public partial class AddModel : Form
+    {
+        public AddModel()
+        {
+            InitializeComponent();
+        }
+        Point lastPoint;
+        private void AddModel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) //Если пользователь зажал левой кнопкой по окну, то он может передвигать окно.
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void AddModel_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);    //Закрепляет позицию после перемещения
+        }
+
+        private void AddModelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
